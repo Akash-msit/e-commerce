2db34b2aa82b9ca73195933cdca1327d9c2236f1
Akash Dey <akash_d.it2018@msit.edu.in>
2023-12-21 11:12:09 +0530

    delete logo

--- src/Customer/Components/Nav/Navbar.jsx
+++ src/Customer/Components/Nav/Navbar.jsx
@@ -300,7 +300,7 @@ export default function Navbar() {
                   <span className="sr-only">Your Company</span>
                   <img
                     className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                    src=""
                     alt=""
                   />
                 </a>
